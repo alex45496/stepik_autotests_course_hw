b552f6e98d283c7fc65fb5f8a6a4851440bec6be
lotov <lotov@t8.ru>
2025-07-27 13:43:29 +0300

    added mark for review

--- test_product_page.py
+++ test_product_page.py
@@ -6,6 +6,7 @@ import pytest
 PRODUCT_BASE_LINK = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207"
 
 
+@pytest.mark.need_review
 @pytest.mark.parametrize('link', [0, 1, 2, 3, 4, 5, 6,
                                   pytest.param(7, marks=pytest.mark.xfail),
                                   8, 9])
@@ -48,6 +49,7 @@ def test_guest_should_see_login_link_on_product_page(browser):
     page.should_be_login_link()
 
 
+@pytest.mark.need_review
 def test_guest_can_go_to_login_page_from_product_page(browser):
     page = ProductPage(browser, PRODUCT_BASE_LINK)
     page.open()
@@ -56,6 +58,7 @@ def test_guest_can_go_to_login_page_from_product_page(browser):
     login_page.should_be_login_page()
 
 
+@pytest.mark.need_review
 def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
     page = ProductPage(browser, PRODUCT_BASE_LINK)
     page.open()
